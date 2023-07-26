927f8b15093535451aa47432e815cce382dc527c
justinait <justiiturraspe@gmail.com>
2023-07-26 19:57:48 -0300

    fix video

--- src/components/Corrpal/Corrpal.js
+++ src/components/Corrpal/Corrpal.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Corrpal.css'
 
 function Corrpal() {
   return (
     <div>
         <h3>Corrpal Systems</h3>
-        <video controls width="640" height="360">
-        <source src='../../Corrpal Systems/corrpal.mp4' type="video/mp4" />
+        <video controls width="640" height="360" className='videoCorrpal'>
+        <source src='../../Corrpal Systems/corrpaldraft.mp4' type="video/mp4" />
         {/* Puedes agregar más formatos de video aquí para compatibilidad con diferentes navegadores */}
         Tu navegador no admite el elemento de video.
       </video>
