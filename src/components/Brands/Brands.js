dbba8c0820358ccebe3d6eadfa23040d6b5d8118
justinait <justiiturraspe@gmail.com>
2023-07-28 17:24:05 -0300

    logos

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -7,17 +7,16 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 function Brands({ brandArray }) {
 
   const [selected, setSelected] = useState('todas');
-  const [selectedBrand, setSelectedBrand] = useState();
 
-  const data = [
-    { name: 'Göpfert', logo: '../../brands/Goepfert.png' },
-    { name: 'Corrpal Systems', logo: './brands/CorrpalSystems.png' },
-    { name: 'Bahmüller', logo: './brands/bahmuller.png' },
-    { name: 'BGM Flexo Folder Gluer', logo: './brands/bgm.jpg' },
-    { name: 'JB Machinery', logo: './brands/JBMachinery.png' },
-    { name: 'Vistron', logo: './brands/Vistron.jpg' },
-    { name: 'Stock Maschinenbau', logo: './brands/StockM.jpg' },
-    { name: 'Minda', logo: './brands/Minda.png' },
+  const dataBranding = [
+    { name: 'Göpfert', logo: '../../brands/goepfert.png' },
+    { name: 'Corrpal Systems', logo: '../../brands/CorrpalSystems.png' },
+    { name: 'Bahmüller', logo: '../../brands/bahmuller.png' },
+    { name: 'BGM Flexo Folder Gluer', logo: '../../brands/bgm.jpg' },
+    { name: 'JB Machinery', logo: '../../brands/JBMachinery.png' },
+    { name: 'Vistron', logo: '../../brands/Vistron.jpg' },
+    { name: 'Stock Maschinenbau', logo: '../../brands/StockM.png' },
+    { name: 'Minda', logo: '../../brands/Minda.png' },
   ];
 
   return (
@@ -28,14 +27,41 @@ function Brands({ brandArray }) {
         const longitud = values.length;
         const brand = values[longitud - 1];
         const machines = values[longitud - 2];
+        
+        let brandToRender = '';
+        let logoBrandToRender= '';
+        switch (brand) {
+          case 'Gopfert':
+            brandToRender = <h3>{dataBranding[0].name}</h3>;
+            logoBrandToRender = <img src={dataBranding[0].logo}/>
+            break;
+          case 'Bahmuller':
+            brandToRender = <h3>{dataBranding[2].name}</h3>;
+            logoBrandToRender = <img src={dataBranding[2].logo}/>
+            break;
+          case 'BGM':
+            brandToRender = <h3>{dataBranding[3].name}</h3>;
+            logoBrandToRender = <img src={dataBranding[3].logo}/>
+            break;
+          case 'JBMachinery':
+            brandToRender = <h3>{dataBranding[4].name}</h3>;
+            logoBrandToRender = <img src={dataBranding[4].logo}/>
+            break;
+          case 'StockMaschinenbau':
+            brandToRender = <h3>{dataBranding[6].name}</h3>;
+            logoBrandToRender = <img src={dataBranding[6].logo}/>
+            break;
 
+            default:
+            brandToRender = <h3>{brand}</h3>
+        }
 
-        console.log(data);
+        console.log(dataBranding[0].logo);
         return (
           <div key={i}>
-            {/* category by machine */}
-            <h3>{brand}</h3>
-
+            {/* <h3>{brand}</h3> */}
+            {brandToRender}
+            {logoBrandToRender}
             <ButtonGroup aria-label="Basic example" className='categoryByMachine'>
               <Button
                 className='machineButton'
