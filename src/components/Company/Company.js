90c9b3a4ba324077c7963ffd2dfe614718d23209
justinait <justiiturraspe@gmail.com>
2023-08-08 18:31:53 -0300

    correcciones

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -5,7 +5,10 @@ function Company() {
   return (
     <div className='company'>
       <p>
-        Somos un equipo joven y dinámico dispuestos a aportar la mejor solución para sus necesidades en impresión (flexográfica y digital) y troquelado. Involucrados desde hace años en la fabricación de clichés, troqueles y maquinaria para el sector del cartón ondulado y en contacto permanente con los fabricantes de maquinaria y componentes nº1 a nivel mundial.
+        Somos un equipo joven y dinámico dispuestos a aportar la mejor solución para sus necesidades en impresión, 
+        flexográfica y troquelado. Involucrados desde hace años en el asesoramiento y venta de maquinaria
+         para el sector del cartón ondulado y en contacto permanente con los fabricantes de maquinaria y componentes
+          nº1 a nivel mundial.
 
       </p>
       
