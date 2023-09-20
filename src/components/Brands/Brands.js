a61b310f4652f529784229a9f0b6456bd75970f7
justinait <justiiturraspe@gmail.com>
2023-09-20 10:48:59 -0300

    swiper corregido

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -80,7 +80,35 @@ function Brands({ brandArray }) {
                 className='swiperBrands'
                 freeMode={false} // Habilitar el modo free (sin bucle)
                 freeModeSticky={false} // Hacer que el tope sea "pegajoso"
-                // simulateTouch={true}
+                breakpoints={{
+                  320: {
+                    slidesPerView: 1.1,
+                    spaceBetween: 10,
+                  },
+                  640: {
+                    slidesPerView: 1.7,
+                    spaceBetween: 5,
+                  },
+                  767: {
+                    slidesPerView: 2,
+                    spaceBetween: 5,
+                  },
+                  1024: {
+                    slidesPerView: 2.3,
+                    spaceBetween: 5,
+                  },
+                  1280: {
+                    slidesPerView: 2.6,
+                    spaceBetween: 10,
+                  },
+                  1536: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+                  },
+                  1700: {
+                    slidesPerView: 3.3,
+                  }
+                }}
               >
                 {valuesExceptLast.map((e, i) => {
                   return(
