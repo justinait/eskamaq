f0ee78f8e696831f5cffce0b4017aa7d19f99c24
justinait <justiiturraspe@gmail.com>
2023-09-20 14:59:01 -0300

    png logos

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -6,7 +6,7 @@ function Company() {
     <div className='company'>
       <img src='./brands.png' className='brands' />
       <p>
-        Somos un equipo joven y dinámico dispuestos a aportar la mejor solución para sus necesidades en impresión, 
+        Somos un equipo jóven y dinámico dispuesto a aportar la mejor solución para sus necesidades en impresión, 
         flexográfica y troquelado. Involucrados desde hace años en el asesoramiento y venta de maquinaria
          para el sector del cartón ondulado y en contacto permanente con los fabricantes de maquinaria y componentes
           nº1 a nivel mundial.
