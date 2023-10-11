3dceafae52ebc3316abeda5ef210ac7a2f27aac9
justinait <justiiturraspe@gmail.com>
2023-10-11 12:54:37 -0300

    correction blur and starting vistron

--- src/components/Vistron/Vistron.js
+++ src/components/Vistron/Vistron.js
@@ -7,11 +7,22 @@ function Vistron() {
     <div>
         <div style={{ textAlign: 'center' }}>
             <a href='https://vistron.com/' target="_blank">
-                <img className='brandingLogo' src='../../brands/Vistron.png' alt='VISTRON'/>
+                <img className='vistronLogo' src='../../brands/Vistron.png' alt='VISTRON'/>
+                {/* className='brandingLogo' */}
             </a>
             
         </div>
-        <div className='vistronContainer'>
+        <div className='vistronIntro'>
+            <p className='vistronTitle'>Tecnología clave para el control de procesos.</p>
+            
+            <p className='vistronDescription'>Vistron GMBH es una empresa operativa a nivel mundial, 
+            <span className='gray'> con sede en Göttingen, Alemania, y con una subsidiaria cerca de Tarragona, España, especializada en el </span>
+            desarrollo, fabricación, distribución y servicio de sistemas de visión artificial industrial para el control y optimización de los procesos de fabricación.
+            </p>
+
+        </div>
+        
+        <div className='vistronInfoContainer'>
 
             <img src='../../Vistron/vistronImagen.png' className='vistronImg'/>
             <p>
