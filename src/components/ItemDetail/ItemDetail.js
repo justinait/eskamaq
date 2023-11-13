fb44d9e9c81bc5cdc7e331d52a0f1332b22d5fe4
justinait <justiiturraspe@gmail.com>
2023-11-13 16:10:28 -0300

    big size

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -25,7 +25,7 @@ function ItemDetail({dataItem, category, allItems}) {
           className='modalSwiper'
           breakpoints={{
             320: {
-              slidesPerView: 2,
+              slidesPerView: 2.4,
               spaceBetween: 10,
             },
             1280: {
